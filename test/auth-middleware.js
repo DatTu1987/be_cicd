a1c0b6b709de251e1bdb03ad870d343c1e6b9e0c
DESKTOP-E21M4B2\Dat Tu <tuchidat1987@gmail.com>
2023-05-21 03:07:20 +0700

    Initialize

---
+++ test/auth-middleware.js
@@ -0,0 +1,42 @@
+const expect = require('chai').expect;
+const authMiddleware = require('../middleware/is-auth');
+const sinon = require('sinon');
+const jwt = require('jsonwebtoken');
+
+describe('Auth Middleware', function() {
+    it('should throw an error if no authorization header is present', function() {
+      const req = {
+        get: function(headerName) {
+          return null;
+        }
+      };
+      expect(authMiddleware.bind(this, req, {}, () => {})).to.throw(
+        'Not authenticated.'
+      );
+    });
+
+    it('should throw an error if the authorization header is only one string', function() {
+      const req = {
+        get: function(headerName) {
+          return 'Bearer xyz';
+        }
+      };
+      expect(authMiddleware.bind(this, req, {}, () => {})).to.throw();
+    });
+
+    it('should yield a userId after decoding the token', function() {
+      const req = {
+        get: function(headerName) {
+          return 'Bearer djfkalsdjfaslfjdlas';
+        }
+      };
+      sinon.stub(jwt, 'verify');
+      jwt.verify.returns({ userId: 'abc' });
+      authMiddleware(req, {}, () => {});
+      expect(req).to.have.property('userId');
+      expect(req).to.have.property('userId', 'abc');
+      expect(jwt.verify.called).to.be.true;
+      jwt.verify.restore();
+    });
+})
+
